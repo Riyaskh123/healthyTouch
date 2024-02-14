4efbe520e552705e76a3fb489d30dc3babcc9d58
riyaskh123 <riyaskh123@gmail.com>
2024-02-14 15:09:52 +0530

    final

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import socketIOClient from "socket.io-client";
 import { useEffect, useState } from 'react';
+import Confetti from 'react-confetti';
+import useWindowSize from "react-use/lib/useWindowSize";
 const ENDPOINT = "http://127.0.0.1:3005/";
 
 function App() {
 
   const [count, setCount] = useState(0);
   const [start, setStart] = useState(false);
+  const [confelli, setConfelli] = useState(false);
   const socket = socketIOClient.connect(ENDPOINT);
 
-
+  const { width, height } = useWindowSize();
   socket.on("start", data => {
     setStart(true);
   });
 
   socket.on("count", data => {
     console.log(data);
+    if (data % 10 === 0) {
+      setConfelli(true)
+      setTimeout(()=>{
+        setConfelli(false)
+      },2000)
+    }
     setCount(data);
   });
 
@@ -27,6 +36,7 @@ function App() {
 
   return (
     <div className="App">
+      <Confetti width={width} height={height} recycle={confelli} />
       <h1>{count}</h1>
       <h2>{start ? 'Start pull up' : 'Thank you visit again' }</h2>
     </div>
